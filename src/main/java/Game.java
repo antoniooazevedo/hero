2d3f4724063861464ef0c1f782ee07e28482c46f
António Azevedo <antazevedo2003@gmail.com>
2022-09-27 12:39:51 +0100

    THE HERO MOVES! also hate switch case

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,6 @@
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,6 +11,8 @@ import java.io.IOException;
 
 public class Game {
     Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     Game(){
         try {
@@ -25,13 +29,42 @@ public class Game {
 
     private void draw() throws IOException {
        screen.clear();
-       screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+       screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
        screen.refresh();
 
     }
 
     public void run() throws IOException{
-        draw();
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q'){
+                screen.close();
+            }
+            if (key.getKeyType() == KeyType.EOF){
+                break;
+            }
+        }
     }
 
+    private void processKey(KeyStroke key){
+       // System.out.println(key);
+       // System.out.println("x and y  " + x + "   " + y);
+        switch (key.getKeyType()){
+            case ArrowUp:
+                y--;
+                break;
+            case ArrowDown:
+                y++;
+                break;
+            case ArrowLeft:
+                x--;
+                break;
+            case ArrowRight:
+                x++;
+                break;
+        }
+
+    }
 }
